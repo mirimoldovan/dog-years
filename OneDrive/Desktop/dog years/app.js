314d6ede2d10716aa2dc759a22b1676ea53509d0
Miriam Moldovan <mirybabutia@yahoo.com>
2021-03-19 00:05:14 +0200

    commited

---
+++ OneDrive/Desktop/dog years/app.js
@@ -0,0 +1,20 @@
+let myAge = 27;
+// my human age
+
+let earlyYears =2;
+//Defining early years
+
+earlyYears *= 10.5;
+
+let laterYears = myAge - 2;
+laterYears *= 4;
+//Defining later years.Each human year following counts as 4 dog years.
+
+let myAgeInDogYears = earlyYears + laterYears;
+//calculating my age in dog years
+
+const = "Miriam" ;
+myName.toLowerCase();
+//my name in lowercase.Log in sentence showing my name, my human age and my dog age
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
